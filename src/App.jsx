b6a7816e86ec7781777c9899e1a0f51efc5bd72f
Sonia Flores Zuñiga <zunigafloressonia105@gmail.com>
2023-03-02 09:11:58 -0600

    Lista-Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const nombre = "Sonia Flores Zuñiga"
+
+  return (
+    <div>
+     <h3>Lista de Hobbies de {nombre}</h3>
+     <ol>
+        <li>Jugar Videojuegos</li>
+        <li>Leer</li>
+        <li>Caminar</li>
+     </ol>
+    </div>
+  )
+}
+
+export default App
